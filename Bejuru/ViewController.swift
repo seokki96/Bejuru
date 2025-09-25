08afdbbe24e8549024d947ed12c786989a152393
seokki96 <kwons3327@gmail.com>
2025-09-25 15:47:14 +0900

    initial commit

---
+++ Bejuru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bejuru
+//
+//  Created by 권석기 on 9/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
